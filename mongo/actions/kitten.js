3b16bec5ee4c50af697a0cbd2ad890bdd9da3200
Evgeniia <e.akulenko@velvetech.ru>
2019-08-21 16:56:04 +0700

    refactored index

--- mongo/actions/kitten.js
+++ mongo/actions/kitten.js
@@ -3,3 +3,13 @@
 //   if (err) return console.error(err);
 //   console.log('qwe');
 // });
+const Kitten = require('../../mongo/schemas/kitten');
+
+function getCats() {
+  return Kitten.find({ name: 'fluffy' }).exec();
+}
+
+
+module.exports = {
+  getCats
+}
--- routes/technique/index.js
+++ routes/technique/index.js
@@ -1,26 +1,12 @@
-const Router = require('koa-router');
-//const router = new Router();
-const Kitten = require('../../mongo/schemas/kitten');
+const Kitten = require('../../mongo/actions/kitten');
 
-function getCats() {
-  //let users = 
-  //yield  Kitten.find({ name: 'fluffy' });
-  return new Promise((resolve, reject) => {
-     const ourCat = Kitten.find({ name: 'fluffy' });
-     ourCat.exec((er, cats) => {
-       if (er) {  reject(er);   }
-       else { resolve(cats); }
-     });        
-  });
- // return user
-}
 
 function TechniquesRoutes(router) {
-  router.get('hoba', '/:id', async (ctx, next) => {
-    const cats = await getCats();
+  router.get('hoba', '/:id',  async (ctx, next) => {
+    const cats = await Kitten.getCats();
     console.log(ctx.params.id + " cats length: " + cats.length);
-  } )
+    ctx.body = ctx.params.id + " cats length: " + cats.length;
+  })
 }
-
 
 module.exports = TechniquesRoutes;
